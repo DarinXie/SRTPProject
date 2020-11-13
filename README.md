c5729d506dac3eed407491f0530fc92904d7af7f
LevsCherish <darin_xie@163.com>
2020-11-13 17:33:56 +0800

    Fix Bug
    解决了跳转的bug，以及更新ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# SRTPProject
- srtp项目
+# SRTP项目
+
+### 环境
+- Tomcat 9
+- Java 13
+- Windows 10
+
--- src/com/srtp/servlet/user/doUserAdd.java
+++ src/com/srtp/servlet/user/doUserAdd.java
@@ -1,15 +1,14 @@
 package com.srtp.servlet.user;
 
 import com.srtp.entity.User;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 
 public class doUserAdd extends HttpServlet {
     @Override
